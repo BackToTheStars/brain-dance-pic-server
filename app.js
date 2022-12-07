c451d06b3a8c3dc2ffd9f1a76c99ba0e6b3d15bc
naukomania <naukomania@gmail.com>
2022-12-07 07:00:29 +0300

    file upload limit increased

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const { staticImagesBasePath } = require('./config/path');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: false }));
 
 app.use('/static/images', express.static(staticImagesBasePath));
 app.use('/images', imagesRouter);
