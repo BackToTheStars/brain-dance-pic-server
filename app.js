7328e194fa203a4d883e2371bee012e377f20df4
naukomania <naukomania@gmail.com>
2022-12-07 07:04:00 +0300

    urlencoded extended set true

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: false }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use('/static/images', express.static(staticImagesBasePath));
 app.use('/images', imagesRouter);
