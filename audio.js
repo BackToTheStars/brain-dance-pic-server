0e9121127b0c16d6964ae9d39c249c0e35cb8f2b
naukomania <naukomania@gmail.com>
2024-08-11 22:37:28 +0300

    audio upload limit updated

--- audio.js
+++ audio.js
@@ -13,8 +13,8 @@ const { AUDIO_HOST } = require('./config/url');
 const { MONGO_URL } = require('./config/db');
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '350mb' }));
+app.use(express.urlencoded({ limit: '350mb', extended: true }));
 
 const storage = multer.memoryStorage();
 const upload = multer({
@@ -86,7 +86,7 @@ app.post(
         src: `${AUDIO_HOST}/audios/${filename}`,
         item: {
           filename,
-        }
+        },
       });
     } catch (error) {
       console.error(error);
